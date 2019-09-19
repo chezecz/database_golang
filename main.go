4fe0107a9d40d505cd440c94f75744914ddd6676
chezecz <cheze92@gmail.com>
2019-09-19 14:38:11 +1000

    separate code in functions

--- main.go
+++ main.go
@@ -6,12 +6,30 @@ import (
 	"time"
 	"go.mongodb.org/mongo-driver/mongo"
     "go.mongodb.org/mongo-driver/mongo/options"
-    "go.mongodb.org/mongo-driver/bson"
+    // "go.mongodb.org/mongo-driver/bson"
 )	
 
+type User struct {
+	user_id int
+	name string
+}
+
 func main() {
+	client, ctx := connect_db()
+
+	collection := get_collection(client)
+
+	res := insert_users(User{1, "Dendi"}, collection, ctx)
+	
+	fmt.Println(res)
+
+	close_connection(client)
+}
+
+func connect_db() (mongo.Client, context.Context) {
 	var err error
-	client, _ := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
 
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	err = client.Connect(ctx)
@@ -20,8 +38,41 @@ func main() {
 		fmt.Println(err)
 	}
 
-	collection := client.Database("testing").Collection("numbers")
-	res, err := collection.InsertOne(ctx, bson.M{"name": "pi", "value": 3.14159})
-	id := res.InsertedID
-	fmt.Println(id)
+	return *client, ctx
+}
+
+func get_collection(client mongo.Client) (mongo.Collection) {
+	collection := client.Database("test_project").Collection("Users")
+	return *collection
+}
+
+func insert_users(user User, collection mongo.Collection, ctx context.Context) (mongo.InsertOneResult) {
+	var err error
+	res, err := collection.InsertOne(ctx, user)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return *res
+}
+
+// func find_users() {
+// 	var result User
+// 	err = collection.FindOne(ctx, filter).Decode(&result)
+
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+
+// 	return result
+// }
+
+func close_connection(client mongo.Client) {
+	var err error
+	err = client.Disconnect(context.TODO())
+
+	if err != nil {
+		fmt.Println(err)
+	}
 }
